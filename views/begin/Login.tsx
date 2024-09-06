2c95822ffff47840d9ebd11c42eb9d330ade84aa
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-06 11:27:47 +0700

    Add useStatusBar custom hook and Login component

---
+++ views/begin/Login.tsx
@@ -0,0 +1,21 @@
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+const Login = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View>
+          <Text>Login</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {},
+});
