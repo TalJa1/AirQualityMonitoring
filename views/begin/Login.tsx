449cb6b2edab5477dd6c03221c3bfc46ed78b088
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-06 12:16:20 +0700

    Refactor package.json and package-lock.json to add react-native-svg dependency

--- views/begin/Login.tsx
+++ views/begin/Login.tsx
@@ -11,14 +11,29 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import {centerAll, containerStyle, vh, vw} from '../../services/styleSheet';
 import useStatusBar from '../../services/useStatusBarCustom';
 import {LoginBtnProps} from '../../services/typeProps';
+import {appleIcon, googleIcon} from '../../assets/svgXml';
 
 const Login = () => {
   useStatusBar('white');
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView style={{paddingHorizontal: vw(5)}}>
-        <View>
+      <ScrollView
+        style={{paddingHorizontal: vw(5)}}
+        contentContainerStyle={{marginVertical: vh(1)}}>
+        <View style={{rowGap: vh(1)}}>
           <BtnLayout title="Sign In" btnColor="#3E3792" textColor="white" />
+          <BtnLayout
+            title="Continue with Google"
+            btnColor="white"
+            textColor="#0000008A"
+            icon={googleIcon(vw(6), vw(6))}
+          />
+          <BtnLayout
+            title="Continue with Apple"
+            btnColor="black"
+            textColor="white"
+            icon={appleIcon(vw(6), vw(6))}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -33,10 +48,18 @@ const BtnLayout: React.FC<LoginBtnProps> = ({
 }) => {
   return (
     <TouchableOpacity
+      id={title}
       style={[
-        {backgroundColor: btnColor, borderRadius: 44, paddingVertical: vh(1)},
+        {
+          backgroundColor: btnColor,
+          borderRadius: 44,
+          paddingVertical: vh(1.5),
+          flexDirection: 'row',
+          columnGap: vw(2),
+        },
         centerAll,
       ]}>
+      {icon}
       <Text style={{fontSize: 20, fontWeight: 700, color: textColor}}>
         {title}
       </Text>
