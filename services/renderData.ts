267e9f56a3367677d8f8267097e876c4f51f348e
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-08 12:11:50 +0700

    Refactor AQI details data structure and add icons
    This commit refactors the AQI details data structure in the renderData.ts file. It imports SVG icons for humidity, sensed temperature, chance of rain, turbidity of clouds, and wind speed. The data is now organized in an array with each item containing an icon, data value, and label. This improves the readability and maintainability of the code.

--- services/renderData.ts
+++ services/renderData.ts
@@ -1,1 +1,36 @@
-// const AQIDetailsData = [{}];
+import {
+  chanceOfRainIcon,
+  humidityIcon,
+  sensedTemperatureIcon,
+  turbidityOfCloudsIcon,
+  windICon,
+} from '../assets/svgXml';
+import {vw} from './styleSheet';
+
+export const AQIDetailsData = [
+  {
+    icon: humidityIcon(vw(7), vw(7)),
+    data: '76%',
+    label: 'Humidity',
+  },
+  {
+    icon: sensedTemperatureIcon(vw(7), vw(7)),
+    data: '28°C',
+    label: 'Sensed Temperature',
+  },
+  {
+    icon: chanceOfRainIcon(vw(7), vw(7)),
+    data: '20%',
+    label: 'Chance of Rain',
+  },
+  {
+    icon: turbidityOfCloudsIcon(vw(7), vw(7)),
+    data: '20%',
+    label: 'Turbidity of Clouds',
+  },
+  {
+    icon: windICon(vw(7), vw(7)),
+    data: '20 km/h',
+    label: 'Wind speed',
+  },
+];
