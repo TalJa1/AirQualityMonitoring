aa8eb61c2a356ae625a287c7acaf9c27745b4a93
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-09 15:21:45 +0700

    Refactor package dependencies to include dayjs library

--- services/renderData.ts
+++ services/renderData.ts
@@ -50,10 +50,10 @@ export const GenerateChartHomeData = (value: string) => {
     const month = currentDate.getMonth();
     const year = currentDate.getFullYear();
 
-    return { month, year };
+    return {month, year};
   };
 
-  const { month, year } = getMonthYear(value);
+  const {month, year} = getMonthYear(value);
 
   const daysInMonth = new Date(year, month + 1, 0).getDate();
 
@@ -104,5 +104,78 @@ export const SliderBottomSaveTabData = [
     img: require('../assets/home/slider4.png'),
     title: 'Seasonal Weather: Climate:',
     description: 'Changes, How to Adapt',
+  },
+];
+
+export const AQIDetailData = [
+  {
+    aqiIndex: 176,
+    img: require('../assets/home/rain.png'),
+    'PM2.5(µg/m³)': 22.4,
+    'PM10(µg/m³)': 34.4,
+    'CO(ppm)': 8.1,
+    'SO2(ppb)': 66.4,
+    'NO2(ppb)': 20.4,
+    'O3(pmm)': 20.4,
+  },
+  {
+    aqiIndex: 85,
+    img: require('../assets/home/rain.png'),
+    'PM2.5(µg/m³)': 12.1,
+    'PM10(µg/m³)': 20.2,
+    'CO(ppm)': 4.5,
+    'SO2(ppb)': 30.2,
+    'NO2(ppb)': 10.1,
+    'O3(pmm)': 15.3,
+  },
+  {
+    aqiIndex: 150,
+    img: require('../assets/home/sun.png'),
+    'PM2.5(µg/m³)': 25.0,
+    'PM10(µg/m³)': 40.0,
+    'CO(ppm)': 7.0,
+    'SO2(ppb)': 50.0,
+    'NO2(ppb)': 15.0,
+    'O3(pmm)': 25.0,
+  },
+  {
+    aqiIndex: 45,
+    img: require('../assets/home/sun.png'),
+    'PM2.5(µg/m³)': 8.0,
+    'PM10(µg/m³)': 12.0,
+    'CO(ppm)': 2.0,
+    'SO2(ppb)': 15.0,
+    'NO2(ppb)': 5.0,
+    'O3(pmm)': 10.0,
+  },
+  {
+    aqiIndex: 120,
+    img: require('../assets/home/rain.png'),
+    'PM2.5(µg/m³)': 18.0,
+    'PM10(µg/m³)': 25.0,
+    'CO(ppm)': 5.0,
+    'SO2(ppb)': 40.0,
+    'NO2(ppb)': 12.0,
+    'O3(pmm)': 20.0,
+  },
+  {
+    aqiIndex: 190,
+    img: require('../assets/home/sun.png'),
+    'PM2.5(µg/m³)': 30.0,
+    'PM10(µg/m³)': 45.0,
+    'CO(ppm)': 9.0,
+    'SO2(ppb)': 60.0,
+    'NO2(ppb)': 18.0,
+    'O3(pmm)': 28.0,
+  },
+  {
+    aqiIndex: 160,
+    img: require('../assets/home/rain.png'),
+    'PM2.5(µg/m³)': 26.0,
+    'PM10(µg/m³)': 38.0,
+    'CO(ppm)': 6.5,
+    'SO2(ppb)': 55.0,
+    'NO2(ppb)': 14.0,
+    'O3(pmm)': 22.0,
   },
 ];
--- views/home/Detail.tsx
+++ views/home/Detail.tsx
@@ -7,7 +7,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {containerStyle, vh, vw} from '../../services/styleSheet';
 import HeaderComponent from '../../components/HeaderComponent';
@@ -15,6 +15,8 @@ import {useRoute} from '@react-navigation/native';
 import {AnimatedCircularProgress} from 'react-native-circular-progress';
 import GradientBackground from '../../components/GradientBackground';
 import {arrowNextIcon, backIcon} from '../../assets/svgXml';
+import {AQIDetailData} from '../../services/renderData';
+import dayjs from 'dayjs';
 
 const getColorFromAqiIndex = (aqiIndex: number): string => {
   if (aqiIndex <= 50) {
@@ -34,12 +36,25 @@ const getColorFromAqiIndex = (aqiIndex: number): string => {
 
 const Detail = () => {
   const route = useRoute();
-  const {subTitle, aqiIndex} = route.params as {
+  const {subTitle} = route.params as {
     subTitle: string;
-    aqiIndex: number;
   };
 
-  const AQIIndexColor = getColorFromAqiIndex(aqiIndex);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex(prevIndex => (prevIndex + 1) % AQIDetailData.length);
+  };
+
+  const handleBack = () => {
+    setCurrentIndex(prevIndex =>
+      prevIndex === 0 ? AQIDetailData.length - 1 : prevIndex - 1,
+    );
+  };
+
+  const currentData = AQIDetailData[currentIndex];
+  const AQIIndexColor = getColorFromAqiIndex(currentData.aqiIndex);
+  const currentDate = dayjs().add(currentIndex, 'day').format('ddd, MM/DD');
 
   return (
     <GradientBackground colors={['white', '#E5FAFD']}>
@@ -47,8 +62,12 @@ const Detail = () => {
         <ScrollView>
           <HeaderComponent title="Detail" isBack={true} subtitle={subTitle} />
           <CircularProgressView
-            aqiIndex={aqiIndex}
+            aqiIndex={currentData.aqiIndex}
             AQIIndexColor={AQIIndexColor}
+            img={currentData.img}
+            date={currentDate}
+            onNext={handleNext}
+            onBack={handleBack}
           />
         </ScrollView>
       </SafeAreaView>
@@ -59,8 +78,12 @@ const Detail = () => {
 const CircularProgressView: React.FC<{
   aqiIndex: number;
   AQIIndexColor: string;
-}> = ({aqiIndex, AQIIndexColor}) => {
-  const fillPercentage = (aqiIndex / 500) * 100;
+  date: string;
+  img: any;
+  onNext: () => void;
+  onBack: () => void;
+}> = ({aqiIndex, AQIIndexColor, date, onNext, onBack, img}) => {
+  const fillPercentage = (aqiIndex / 200) * 100;
 
   return (
     <View
@@ -70,7 +93,7 @@ const CircularProgressView: React.FC<{
         alignItems: 'center',
         justifyContent: 'space-evenly',
       }}>
-      <TouchableOpacity style={styles.btnCircularStyle}>
+      <TouchableOpacity style={styles.btnCircularStyle} onPress={onBack}>
         {backIcon(vw(7), vw(7), '#D5CFF9')}
       </TouchableOpacity>
       <AnimatedCircularProgress
@@ -87,16 +110,16 @@ const CircularProgressView: React.FC<{
               width={vw(10)}
               height={vw(10)}
               resizeMode="contain"
-              source={require('../../assets/home/rain.png')}
+              source={img}
             />
-            <Text style={styles.dayTxt}>Today</Text>
+            <Text style={styles.dayTxt}>{date}</Text>
             <Text style={[styles.aqiTxt, {color: AQIIndexColor}]}>
               {aqiIndex}
             </Text>
           </View>
         )}
       </AnimatedCircularProgress>
-      <TouchableOpacity style={styles.btnCircularStyle}>
+      <TouchableOpacity style={styles.btnCircularStyle} onPress={onNext}>
         {arrowNextIcon(vw(7), vw(7), '#D5CFF9')}
       </TouchableOpacity>
     </View>
