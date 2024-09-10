52466910472e05ef5316a6a7865d0838f05777d9
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-10 09:10:48 +0700

    Refactor svgXml.tsx to add inforCircleIcon component

--- assets/svgXml.tsx
+++ assets/svgXml.tsx
@@ -9,6 +9,17 @@ export const test = (w?: any, h?: any, color?: any) => {
   return <SvgXml xml={xml} width={w} height={h} />;
 };
 
+export const inforCircleIcon = (w?: any, h?: any, color?: any) => {
+  const xml = `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10 5.01001V5.00002M10 15L10 8M19 10C19 14.9706 14.9706 19 10 19C5.02944 19 1 14.9706 1 10C1 5.02944 5.02944 1 10 1C14.9706 1 19 5.02944 19 10Z" stroke=${
+    color ?? '#6E778B'
+  } stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+
+  return <SvgXml xml={xml} width={w} height={h} />;
+};
+
 export const desceasingIcon = (w?: any, h?: any, color?: any) => {
   const xml = `<svg width="20" height="12" viewBox="0 0 20 12" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M19 11L12.4142 4.41421C11.6332 3.63316 10.3668 3.63317 9.58579 4.41421L8.41421 5.58579C7.63316 6.36684 6.36683 6.36683 5.58579 5.58579L1 1M19 11H13M19 11V5" stroke=${
--- views/main/Rank.tsx
+++ views/main/Rank.tsx
@@ -1,19 +1,58 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import {containerStyle} from '../../services/styleSheet';
+import React, {useState} from 'react';
+import {containerStyle, vw} from '../../services/styleSheet';
 import useStatusBar from '../../services/useStatusBarCustom';
+import HeaderComponent from '../../components/HeaderComponent';
+import { Picker } from '@react-native-picker/picker';
+import { inforCircleIcon } from '../../assets/svgXml';
 
 const Rank = () => {
   useStatusBar('white');
+  const [headerData, setHeaderData] = useState({
+    title: 'Hanoi',
+    subTitle: 'Hoan Kiem, Hanoi',
+  });
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <View>
-          <Text>Rank</Text>
-        </View>
+        <HeaderComponent
+          title={headerData.title}
+          subtitle={headerData.subTitle}
+          isBack={false}
+        />
+        <DateSelectionView />
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const DateSelectionView: React.FC = () => {
+  return (
+    <View
+      style={{
+        paddingHorizontal: vw(5),
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+      }}>
+        {/* <Picker
+            dropdownIconColor={'#6E778B'}
+            selectedValue={}
+            style={{
+              width: vw(34),
+              color: '#272727',
+            }}
+            onValueChange={itemValue => setSelectedMonth(itemValue)}>
+            <Picker.Item label={getMonthYearHomeChart(0)} value="current" />
+            <Picker.Item label={getMonthYearHomeChart(-1)} value="previous1" />
+            <Picker.Item label={getMonthYearHomeChart(-2)} value="previous2" />
+            <Picker.Item label={getMonthYearHomeChart(-3)} value="previous3" />
+          </Picker> */}
+          {inforCircleIcon(vw(6), vw(6))}
+      </View>
   );
 };
 
