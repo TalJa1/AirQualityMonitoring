34a7b1594f4455bab75efe49eee0792ca0b8b427
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-10 14:07:39 +0700

    Refactor Map.tsx to update containerStyle import, add headerTitle state, and improve component rendering

--- views/main/Map.tsx
+++ views/main/Map.tsx
@@ -1,25 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {containerStyle} from '../../services/styleSheet';
 import useStatusBar from '../../services/useStatusBarCustom';
+import HeaderComponent from '../../components/HeaderComponent';
+import GradientBackground from '../../components/GradientBackground';
 
 const Map = () => {
   useStatusBar('white');
+  const [headerTitle, setHeaderTitle] = useState('Hanoi, Vietnam');
 
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View>
-          <Text>Map</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <GradientBackground colors={['white', '#E5FAFD']}>
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <HeaderComponent title={headerTitle} isBack={false} />
+          <View>
+            <Text>Map</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </GradientBackground>
   );
 };
 
 export default Map;
 
 const styles = StyleSheet.create({
-  container: containerStyle,
+  container: {flex: 1},
 });
